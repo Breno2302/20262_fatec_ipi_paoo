dee7fa5c253b4410dfa94f58cd78f2f35e6f91dd
Gustavo Fambrini <gustavotoneto1@gmail.com>
2026-08-05 16:20:56 -0300

    learn: sistema de tipos

--- intro_javascript/script.js
+++ intro_javascript/script.js
@@ -1,1 +1,4 @@
-// começando...
+// começando...//
+//const, let, var
+// a = 2; a variável a está appntando para um objeto de tipo number
+// a = "abc"; // mudou agora a variável a está appntando paera um objeto de tipo string
