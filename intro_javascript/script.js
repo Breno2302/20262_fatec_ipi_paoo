bbb60c698772f9ca3ec3384632c9ce64517842c8
Breno <brenotoneto1@gmail.com>
2026-08-05 16:46:18 -0300

    lean: sistema de tipos

--- intro_javascript/script.js
+++ intro_javascript/script.js
@@ -2,9 +2,3 @@
 //const, let, var
 // a = 2; a variável a está appntando para um objeto de tipo number
 // a = "abc"; // mudou agora a variável a está appntando paera um objeto de tipo string
-//const nome = 'Breno'
-//console.log(nome)
-var nome = 'José'
-console.log(nome)
-nome = "João"
-console.log(nome)
