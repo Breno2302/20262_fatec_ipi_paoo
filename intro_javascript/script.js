ad6df28fad1e6b8aadc56ae3bd4bccda728df67f
Breno <brenotoneto1@gmail.com>
2026-08-05 17:39:14 -0300

    learn: dupla passagem

--- intro_javascript/script.js
+++ intro_javascript/script.js
@@ -1,10 +1,29 @@
 // começando...//
+
 //const, let, var
 // a = 2; a variável a está appntando para um objeto de tipo number
 // a = "abc"; // mudou agora a variável a está appntando paera um objeto de tipo string
+
+//uso do const
 //const nome = 'Breno'
 //console.log(nome)
-var nome = 'José'
-console.log(nome)
-nome = "João"
-console.log(nome)
+
+//uso do var
+//var nome = 'José'
+//console.log(nome)
+//nome = "João"
+//console.log(nome)
+
+//var linguagem = "javascript"
+//console.log("Aprendendo " + linguagem)
+//var linguagem = "java"
+//console.log("Aprndendo " + linguagem)
+
+//hoist: içamento
+let idade = 18
+console.log("oi, " + nome)
+if(idade >= 18){
+    let nome = "João"
+    console.log("Parabéns, " + nome + ". Você pode dirigir")
+}
+console.log("Até mais, " + nome)
